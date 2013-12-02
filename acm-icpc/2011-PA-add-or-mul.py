d6c1538d3d1aa53ce3de4675c53f16a22c932619
Sergio de Mingo <sdemingo@gmail.com>
2013-12-02 20:05:41 +0100

    Finish the Problem A of ACM 2011. Not checked yet

--- acm-icpc/2011-PA-add-or-mul.py
+++ acm-icpc/2011-PA-add-or-mul.py
@@ -3,6 +3,12 @@ Problem A
 To Add or to Multiply
 '''
 
+import sys
+
+
+# Clase para recoger todos las posibles soluciones. Cuando construyo un
+# nodo puedo ejecutar su método build. Esto provocará que se construyana
+# a su vez sus hijos de forma recursiva.
 
 class Tree():
 
@@ -52,6 +58,14 @@ class Tree():
 
           
 
+# Recorro el árbol buscando nodos exitosos. Para cada nodo que recorro
+# apunto su codigo de instrucción en el programa, cuando vuelvo a el,
+# acabada la exploración de sus hijos y retorno hacia el padre, quito
+# ese codigo de operación.
+#
+# Cuando encuentro un caso de éxito clono el contenido del buffer
+# programa en ese momento a la variable pcopy y la adjunto a la lista de
+# programas exitosos.
 
 def walkTree(succs,program,tree):
 
@@ -70,33 +84,101 @@ def walkTree(succs,program,tree):
 
     program.pop()
         
+
+
+
+# Comprime el programa en forma de lista de Aes y Mes en un string
+# tal y como se indica en el enunciado
+
+def compressProgram(program):
+    p=""
+    i=0
+    m=0
+    a=0
+    while (i<len(program)):
+        if (program[i]=="M"):
+            m+=1
+            if (a>0):
+                p=p+str(a)+"A "
+                a=0
+            
+        if (program[i]=="A"):
+            a+=1
+            if (m>0):
+                p=p+str(m)+"M "
+                m=0
+        i+=1
+
+    if (a>0):
+        p=p+str(a)+"A"
+        a=0
+    if (m>0):
+        p=p+str(m)+"M"
+        m=0
+
+    return p
         
 
 
 def main():
-    #a m p q r s
-    #1 2 2 3 10 20
-    config={'a':1,'m':2,'p':2,'q':3,'r':10,'s':20}
+    tests=[]
+    finput=open("2011-PA-add-or-mul.input")
+    for line in finput.readlines():
+        numbers=line.strip().split(" ")
+        numbers=list(map(int,numbers))
+        if max(numbers)==0:
+            break
+        config={}
+        config['a']=numbers[0]
+        config['m']=numbers[1]
+        config['p']=numbers[2]
+        config['q']=numbers[3]
+        config['r']=numbers[4]
+        config['s']=numbers[5]
+        tests.append(config)
+
+    # Una vez leidos todos los test empeizo a procesar
+
+    ntest=1
+    for config in tests:
+        output=[config['p'],config['q']]
+        program=[]
+        success=[]
+
+        tree=Tree(config,output,"")
+        tree.build()
+
+        walkTree(success,program,tree)
+
+        # Calculo la longitud mínima
+        minlen=sys.maxsize
+        for p in success:
+            if (len(p)<=minlen):
+                minlen=len(p)
+
+        # Solo me quedo con los que tienen esa longitu mínima
+        programs=[] 
+        for p in success:
+            if len(p)==minlen:
+                programs.append(compressProgram(p))
 
 
-    output=[2,3]
-    program=[]
-    success=[]
+        programs.sort()
 
-    tree=Tree(config,output,"")
-    tree.build()
-    print ("Arbol construido")
+        #print (programs)
 
-    walkTree(success,program,tree)
-
-    # comprimo los programas de success en strings y los ordeno
-    programs=[]
-    for p in success:
-        programs.append(" ".join(p))
-
-    print (programs)
- 
-
+        msg=""
+        if (len(programs)==0):
+            msg="impossible"
+        else:
+            best=programs[0]
+            if len(best)==0:
+                msg="empty"
+            else:
+                msg=best
+        
+        print ("Case "+str(ntest)+": "+msg)
+        ntest+=1
 
 
 
