f2d7f8801204d6ed8f6429839a11d3ab0753afb8
Sergio de Mingo <sdemingo@gmail.com>
2013-12-13 19:20:41 +0100

    Finish euler prlbem 19

--- euler/euler019.py
+++ euler/euler019.py
@@ -1,6 +1,21 @@
 '''
 Problem 19
+
+You are given the following information, but you may prefer to do some research for yourself.
+
+    1 Jan 1900 was a Monday.
+    Thirty days has September,
+    April, June and November.
+    All the rest have thirty-one,
+    Saving February alone,
+    Which has twenty-eight, rain or shine.
+    And on leap years, twenty-nine.
+    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
 '''
+
 
 months=[0,31,28,31,30,31,30,31,31,30,31,30,31]
 week=["L","M","X","J","V","S","D"]
@@ -13,19 +28,15 @@ def isLeap(year):
         return False
 
 
-# def firstDayOfMonths():
-#     s=1  #primer dia del año fue martes (2)
 
-#     print ("El 1/1 fue "+week[s])
-#     for m in range(1,12):
-#         f=(s+months[m])%7
-#         s+=months[m]
-#         print ("El 1/"+str(m+1)+" fue "+week[f])
-    
 
 def countSundays(year,firstDay):
     s=firstDay
     sundays=0
+
+    if (s==6):    # Si el primer dia del año es domingo, incremento
+        print ("El 1/1 fue "+week[s])
+        sundays+=1
 
     for m in range(1,12):
         f=(s+months[m])%7
@@ -38,25 +49,25 @@ def countSundays(year,firstDay):
             s+=1 #uno mas a febrero por ser bisiesto
 
     return sundays
-    
+
 
 
 
 
 def main():
-    suns=0
+    suns=1
     s=1 # 1 Por el martes de 1/1/1901
     for y in range(1901,2001):
         first=s%7
-        print ("Primer día del "+str(y)+" fue "+week[first])
-        suns+=countSundays(y,first)
+        print ("Año: "+str(y)+":")
+        d=countSundays(y,first)
+        suns+=d
+        print ("\t\t\t domingos: "+str(d)+" suns: "+str(suns))
         s+=365
         if isLeap(y):
             s+=1
         
     print (suns)
-
-
 
 
 
